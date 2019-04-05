7354d6a362d51b57b2e8d8a7af830371dcab55c2
Jakub Pavlik <severus@post.cz>
2019-04-05 20:34:35 +0200

    add lambdish/phunctional to the comparison
    + colourful output clearly showing how complete which implementation is

---
+++ tests/PhunctionalTest.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace Tests;
+
+use Lambdish\Phunctional as p;
+use Tests\Toys\Route;
+
+/**
+ * lambdish/phunctional
+ */
+class PhunctionalTest extends BaseTestCase
+{
+    public function multiply_by_two(array $input)
+    {
+        return p\map(
+            function (int $i) {
+                return $i * 2;
+            },
+            $input
+        );
+    }
+
+    public function routes_to_unique_cities(array $input)
+    {
+        return p\pipe(
+            p\partial('Lambdish\Phunctional\map', function (Route $route) {
+                return [$route->getFrom(), $route->getTo()];
+            }),
+            'Lambdish\Phunctional\flatten',
+            'array_unique',
+            'array_values' // reset keys
+        )($input);
+    }
+
+    public function class_to_method_name(string $className)
+    {
+        return p\pipe(
+            p\partial('explode', '\\'),
+            'Lambdish\Phunctional\last',
+            'lcfirst'
+        )($className);
+    }
+
+    public function timestamps_to_seconds(array $timestamps)
+    {
+        return p\map(
+            p\pipe(
+                function ($i) {
+                    // The "identity function" at the beginning of the pipe has a purpose:
+                    // `map` passes two arguments to it's callback and both are passed
+                    // to the first function in the pipe.
+                    // if it's partially applied `explode`, the second argument (index)
+                    // will be used as splitting limit, which will break the algorithm.
+                    // Try to comment this function out and see.
+                    return $i;
+                },
+                p\partial('explode', ':'),
+                p\partial('Lambdish\Phunctional\map', function (string $numStr) {
+                    return (int)$numStr;
+                }),
+                'array_reverse',
+                p\partial('Lambdish\Phunctional\map', function (int $num, int $index) {
+                    return $num * (60 ** $index);
+                }),
+                'array_sum'
+            ),
+            $timestamps
+        );
+    }
+}
